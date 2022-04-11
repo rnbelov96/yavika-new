1b7cb374d140526e377a238c4bfa3f4283706a94
Belov Roman <rnbelov96@gmail.com>
2022-04-11 18:06:43 +0300

    Make last edits

--- src/ts/f5leads-init.js
+++ src/ts/f5leads-init.js
@@ -1,6 +1,6 @@
 document.f5leads = {};
 document.f5leads.emails = 'franch5.leads@gmail.com, leads@conversionpro.agency'; // comma separated
-document.f5leads.send_to_bitrix = 'false';
+document.f5leads.send_to_bitrix = 'true';
 document.f5leads.expect_second_form = '0';
 document.f5leads.counterId = '';
 
